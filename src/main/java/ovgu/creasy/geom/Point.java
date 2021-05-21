2e87e6984e64b453c513c7deebac4a20476a1806
xkevio <xkeviota@gmail.com>
2021-05-21 02:07:05 +0200

    Setup Point class with boilerplate code

--- src/main/java/ovgu/creasy/geom/Point.java
+++ src/main/java/ovgu/creasy/geom/Point.java
@@ -4,4 +4,27 @@ package ovgu.creasy.geom;
  * 2d Point with double precision floating point coordinates
  */
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
