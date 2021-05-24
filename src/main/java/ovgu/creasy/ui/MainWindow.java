43174a4fabfa9e23a6b6ef79d2d65b1118c735ab
undertrox <undertrox@gmail.com>
2021-05-25 00:08:53 +0200

    Added a Menu Bar and Canvas to the MainWindow and created OrigamiModel for bundling a Crease Pattern with it's Diagram

--- src/main/java/ovgu/creasy/ui/MainWindow.java
+++ src/main/java/ovgu/creasy/ui/MainWindow.java
@@ -1,4 +1,22 @@
 package ovgu.creasy.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import ovgu.creasy.origami.OrigamiModel;
+
 public class MainWindow {
+    public Canvas mainCanvas;
+    private OrigamiModel model;
+
+    @FXML
+    public void onMenuImportAction(ActionEvent actionEvent) {
+        System.out.println("Import");
+        model = new OrigamiModel();
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
 }
