fd553b43d83f5d2077285e9258d24ec6294953ea
undertrox <undertrox@gmail.com>
2021-05-25 00:33:12 +0200

    Added fields to origami classes

--- src/main/java/ovgu/creasy/origami/Diagram.java
+++ src/main/java/ovgu/creasy/origami/Diagram.java
@@ -1,7 +1,28 @@
 package ovgu.creasy.origami;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * A collection of steps to get to a set crease pattern
  */
 public class Diagram {
+    /**
+     * The sequence of steps that is currently selected,
+     * starting with the initial step (from the fully folded cp).
+     * The contents of this list should always form a Step sequence,
+     * meaning that one Step's to field should be equal to the next Step's from field
+     */
+    private List<DiagramStep> selectedSteps;
+    /**
+     * all steps that have been discovered so far
+     */
+    private Collection<DiagramStep> allSteps;
+
+    public Diagram() {
+        this.selectedSteps = new ArrayList<>();
+        this.allSteps = new HashSet<>();
+    }
 }
--- src/main/java/ovgu/creasy/origami/DiagramStep.java
+++ src/main/java/ovgu/creasy/origami/DiagramStep.java
@@ -1,8 +1,20 @@
 package ovgu.creasy.origami;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * A single step that transforms one crease pattern into a slightly
- * different one using some folding technique
+ * different one using some folding technique, equivalent to an edge in the
+ * Step sequence graph
  */
 public class DiagramStep {
+    /**
+     * The cp before the simplification step is applied (usually more complex than to)
+     */
+    private ExtendedCreasePattern from;
+    /**
+     * the cp after the simplification step is applied (usually less complex than from)
+     */
+    private ExtendedCreasePattern to;
 }
