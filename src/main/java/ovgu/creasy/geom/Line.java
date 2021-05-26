be0811ab07bcf7ef8e3c98f5bd3fa9e9fb08cefd
xkevio <xkeviota@gmail.com>
2021-05-26 14:42:27 +0200

    Setup Line class with getPointAt method and added toString() to Point

--- src/main/java/ovgu/creasy/geom/Line.java
+++ src/main/java/ovgu/creasy/geom/Line.java
@@ -4,4 +4,43 @@ package ovgu.creasy.geom;
  * 2d Line segment
  */
 public class Line {
+    private Point start;
+    private Point end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     *  Returns a Point between the start and end point of the line
+     *  at factor t
+     * @param t a factor between 0 and 1 [0,1]
+     * @return the Point at factor t between start and end of the line
+     */
+    public Point getPointAt(double t) {
+        if (t < 0 || t > 1) throw new ArithmeticException("t should be between 0 and 1!");
+
+        Point at = new Point(0, 0);
+        at.setX(start.getX() + t * end.getX());
+        at.setY(start.getY() + t * end.getY());
+
+        return at;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start = start;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
 }
--- src/main/java/ovgu/creasy/geom/Point.java
+++ src/main/java/ovgu/creasy/geom/Point.java
@@ -27,4 +27,12 @@ public class Point {
     public void setY(double y) {
         this.y = y;
     }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
