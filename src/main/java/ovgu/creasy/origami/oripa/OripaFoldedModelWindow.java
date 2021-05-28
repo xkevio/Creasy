7099d40a59e854bed6eef7e2ca39946f899956d1
undertrox <undertrox@gmail.com>
2021-05-28 16:57:58 +0200

    Add Wrapper class for Oripa's Window to show the folded model

---
+++ src/main/java/ovgu/creasy/origami/oripa/OripaFoldedModelWindow.java
@@ -0,0 +1,62 @@
+package ovgu.creasy.origami.oripa;
+
+import oripa.domain.cptool.LineAdder;
+import oripa.domain.creasepattern.CreasePatternFactory;
+import oripa.domain.creasepattern.CreasePatternInterface;
+import oripa.domain.fold.FoldedModel;
+import oripa.domain.fold.Folder;
+import oripa.domain.fold.foldability.FoldabilityChecker;
+import oripa.domain.fold.halfedge.OrigamiModel;
+import oripa.domain.fold.halfedge.OrigamiModelFactory;
+import oripa.domain.fold.subface.FacesToCreasePatternConverter;
+import oripa.domain.fold.subface.ParentFacesCollector;
+import oripa.domain.fold.subface.SplitFacesToSubFacesConverter;
+import oripa.domain.fold.subface.SubFacesFactory;
+import oripa.view.estimation.EstimationResultFrame;
+import ovgu.creasy.origami.CreasePattern;
+
+import javax.swing.*;
+
+public class OripaFoldedModelWindow {
+    private CreasePatternInterface cp;
+    private OrigamiModel model;
+    private FoldedModel foldedModel;
+    private JFrame window;
+    private FoldabilityChecker foldabilityChecker;
+    private Folder folder;
+
+    public OripaFoldedModelWindow(CreasePattern cp) {
+        this.cp = OripaTypeConverter.convertToOripaCp(cp);
+        window = new EstimationResultFrame();
+        folder = new Folder(
+                new SubFacesFactory(
+                        new FacesToCreasePatternConverter(
+                                new CreasePatternFactory(),
+                                new LineAdder()),
+                        new OrigamiModelFactory(),
+                        new SplitFacesToSubFacesConverter(),
+                        new ParentFacesCollector()));
+        foldabilityChecker = new FoldabilityChecker();
+    }
+
+    public boolean foldModel() {
+        model = new OrigamiModelFactory().createOrigamiModel(cp, cp.getPaperSize());
+        if (foldabilityChecker.testLocalFlatFoldability(model)) {
+            foldedModel = folder.fold(model, true);
+            int solutionNum = foldedModel.getFoldablePatternCount();
+            if (solutionNum < 1) return false;
+        }
+        return false;
+    }
+
+    public void show() {
+        EstimationResultFrame frame = new EstimationResultFrame();
+        frame.setModel(foldedModel);
+        if (window != null) {
+            window.dispose();
+        }
+        window = frame;
+        frame.repaint();
+        frame.setVisible(true);
+    }
+}
