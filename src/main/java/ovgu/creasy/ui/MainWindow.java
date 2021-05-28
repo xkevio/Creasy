932f9e35da587c924968cbb2de0d94a7d91531e9
xkevio <xkeviota@gmail.com>
2021-05-28 12:27:34 +0200

    Switched to JavaFX FileChooser

--- src/main/java/ovgu/creasy/ui/MainWindow.java
+++ src/main/java/ovgu/creasy/ui/MainWindow.java
@@ -1,54 +1,41 @@
 package ovgu.creasy.ui;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.stage.FileChooser;
 import ovgu.creasy.origami.OrigamiModel;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class MainWindow extends Component {
+public class MainWindow  {
     public Canvas mainCanvas;
     private OrigamiModel model;
-    private ActionEvent actionEvent;
-    private final JFileChooser openFileChooser;
+
+    private final FileChooser openFileChooser;
     private String filePath = "";
     private String lastPath = "";
 
     public MainWindow() {
-        openFileChooser = new JFileChooser(lastPath);
-        openFileChooser.setFileFilter(new FileNameExtensionFilter("Crease Pattern","cp"));
+        openFileChooser = new FileChooser();
+        openFileChooser.setTitle("Open .cp File");
+        openFileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Crease Patterns", "*.cp"));
     }
 
     @FXML
-    public void onMenuImportAction(ActionEvent actionEvent) {
-        int returnValue = openFileChooser.showOpenDialog(this);
-        // TODO: Checken ob eine .cp Datei ausgewählt wurde, wenn nein --> Fehlermeldung
-        if (returnValue == JFileChooser.APPROVE_OPTION){
-            try {
-                filePath = openFileChooser.getSelectedFile().getPath();
-                lastPath = filePath;
-                write(lastPath);
-                System.out.println("Import completed! Filename: " + openFileChooser.getSelectedFile().getName());
-                // TODO: Crease Pattern via Oripa einfügen (Klassenaufruf + loadCP(filePath))
-            }catch (Exception e) {
-                JOptionPane.showMessageDialog(
-                        this, e.toString(), "Error_FileLoadFailed",
-                        JOptionPane.ERROR_MESSAGE);
-                e.printStackTrace();
-                System.out.println("Error to import your file!");
-            }
-        }
-        else {
-            System.out.println("No file selected!");
+    public void onMenuImportAction() {
+        File file = openFileChooser.showOpenDialog(mainCanvas.getScene().getWindow());
+        if (file.exists()) {
+            // TODO read file and check if its a valid .cp
+            // Perhaps make a static createFromFile(File file) method in CreasePattern
+        } else {
+            System.err.println("No file selected!");
         }
     }
 
+    // probably not needed
     public static void write(String lastPath) {
         FileWriter fw = null;
         try {
@@ -63,7 +50,5 @@ public class MainWindow extends Component {
     }
 
     @FXML
-    private void initialize() {
-
-    }
+    private void initialize() {}
 }
