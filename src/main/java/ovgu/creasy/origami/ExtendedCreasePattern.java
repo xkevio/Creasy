376cdb56f55d0c4cb7dd60c5cf0fa2e5e27a78a7
Nils Seifert <nils.seifert@st.ovgu.de>
2021-06-18 21:05:00 +0200

    added getter & setter + first try to implement 'buildExtendedGraph'

--- src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
+++ src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
@@ -1,9 +1,9 @@
 package ovgu.creasy.origami;
 
+import ovgu.creasy.geom.Point;
 import ovgu.creasy.geom.Vertex;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Extended Crease Pattern, adds information about Reflection Creases
@@ -17,7 +17,7 @@ public class ExtendedCreasePattern {
 
     /**
      * @param xV is the set of extended vertices
-     * @param xC is the set of directed edges of the extended graph. Each xC ist called extended creases.
+     * @param xC is the set of directed edges of the extended graph. Each xC ist called extended creases
      * @param xL is a set of ordered, circular lists of edges parting from each vertex xV
      */
     public ExtendedCreasePattern(Set<Vertex> xV, Set<ExtendedCrease> xC, Set<List> xL) {
@@ -29,17 +29,86 @@ public class ExtendedCreasePattern {
     public ExtendedCreasePattern() {
     }
 
-    public ExtendedCreasePattern buildExtendedGraph(CreasePattern cp) {
-        ExtendedCreasePattern xCP = new ExtendedCreasePattern();
-        ReflectionGraphFactory reflGraph = new ReflectionGraphFactory(cp);
-        /**
-         * TODO:
-         * copy each Vertex (from given CP) to extended Vertex
-         * From each Crease (from given CP) construct xC1 & xC2 with default active=false
-         * construct the set of lists xL so that it reflects the same topology as in the given CP
-         * initialize set of all reflection graphs
-         */
+    public Set<Vertex> getVertices() {
+        return Collections.unmodifiableSet(xV);
+    }
 
+    public Set<ExtendedCrease> getCreases() {
+        return Collections.unmodifiableSet(xC);
+    }
+
+    public Set<List> getListOfVertices() { return Collections.unmodifiableSet(xL); }
+
+    public ExtendedCreasePattern buildExtendedGraph(CreasePattern cp) {
+        ReflectionGraphFactory reflGraph = new ReflectionGraphFactory(cp);
+        Point defaultPoint = new Point(0,0);
+        // copy each Vertex (from given CP) to extended Vertex
+        Set<Vertex> vertices = copyVertices(cp);
+        // From each Crease (from given CP) construct extended Crease
+        Set<ExtendedCrease> extendedCreases = createExtendedCreases(cp, false);
+        Set<ExtendedCrease> extendedCreasesReversed = createExtendedCreases(cp, true);
+        // construct a set of ordered circular lists of edges parting from each vertex xV
+        Set<List> xL = createList(vertices);
+        // set of all reflection graphs
+        Collection<ReflectionGraph> reflectionGraphs = reflGraph.getAllReflectionGraphs();
+
+        for (ReflectionGraph reflectionGraph : reflectionGraphs) {
+            // set of local maximum reflection paths in reflectionGraph
+            Collection<ReflectionPath> A = ReflectionGraphFactory.getLocalMaxima(reflectionGraph);
+            // global maximum in A
+            ReflectionPath y = getGlobalMaxima(A);
+            // terminal vertices of y TODO
+            Vertex x1, x2;
+            // Extended Crease in y with xC1.getStartVertex() == xV1 TODO
+            ExtendedCrease xC1 = null;
+            // Extended Crease in y with xC2.getStartVertex() == xV2 TODO
+            ExtendedCrease xC2 = null;
+            // activate xC1 and xC2
+            xC1.setActive(true);
+            xC2.setActive(true);
+
+            Vertex xV1 = null, xV2 = null;
+
+            if (xV1.getPoint() == xV2.getPoint() && xC1.getType() != xC2.getType()) {
+                Vertex newVertex = new Vertex(defaultPoint, Vertex.Type.VIRTUAL);
+                xC1.setEndVertex(newVertex);
+                ExtendedCrease new_xC1 = new ExtendedCrease(newVertex, xV1, xC1.getType(), true);
+                xC2.setEndVertex(newVertex);
+                ExtendedCrease new_xC2 = new ExtendedCrease(newVertex, xV2, xC1.getType(), true);
+                vertices.add(newVertex);
+                xC.add(new_xC1);
+                xC.add(new_xC2);
+            } else {
+                xC1.setEndVertex(xV2);
+                xC2.setEndVertex(xV1);
+            }
+        }
+        ExtendedCreasePattern xCP = new ExtendedCreasePattern(xV, xC, xL);
         return xCP;
+    }
+    // TODO
+    private Set<List> createList(Set<Vertex> vertices) {
+        Set<List> lists = new HashSet<>();
+        return lists;
+    }
+
+    // TODO
+    private Set<ExtendedCrease> createExtendedCreases(CreasePattern cp, boolean reverse) {
+        Set<ExtendedCrease> xC = null;
+        // if reverse == false --> xC = (v1, v2, a, false)
+        // if reverse == true --> xC = (v2, v1, a, false)
+        return xC;
+    }
+
+    // TODO
+    private Set<Vertex> copyVertices(CreasePattern cp) {
+        Set<Vertex> vertices = null;
+        return vertices;
+    }
+
+    // TODO
+    private ReflectionPath getGlobalMaxima(Collection<ReflectionPath> reflectionPath) {
+        ReflectionPath globalMaxima = null;
+        return globalMaxima;
     }
 }
