b853a0889da67952771d808d742576b7698459aa
xkevio <xkeviota@gmail.com>
2021-07-10 18:40:34 +0200

    Parameterize generic types

--- src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
+++ src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
@@ -13,14 +13,14 @@ import java.util.*;
 public class ExtendedCreasePattern {
     private Set<Vertex> xV;
     private Set<ExtendedCrease> xC;
-    private Set<List> xL;
+    private Set<List<Vertex>> xL;
 
     /**
      * @param xV is the set of extended vertices
      * @param xC is the set of directed edges of the extended graph. Each xC ist called extended creases
      * @param xL is a set of ordered, circular lists of edges parting from each vertex xV
      */
-    public ExtendedCreasePattern(Set<Vertex> xV, Set<ExtendedCrease> xC, Set<List> xL) {
+    public ExtendedCreasePattern(Set<Vertex> xV, Set<ExtendedCrease> xC, Set<List<Vertex>> xL) {
         this.xV = xV;
         this.xC = xC;
         this.xL = xL;
@@ -37,18 +37,22 @@ public class ExtendedCreasePattern {
         return Collections.unmodifiableSet(xC);
     }
 
-    public Set<List> getListOfVertices() { return Collections.unmodifiableSet(xL); }
+    public Set<List<Vertex>> getListOfVertices() { return Collections.unmodifiableSet(xL); }
 
     public ExtendedCreasePattern buildExtendedGraph(CreasePattern cp) {
         ReflectionGraphFactory reflGraph = new ReflectionGraphFactory(cp);
         Point defaultPoint = new Point(0,0);
+
         // copy each Vertex (from given CP) to extended Vertex
         Set<Vertex> vertices = copyVertices(cp);
+
         // From each Crease (from given CP) construct extended Crease
         Set<ExtendedCrease> extendedCreases = createExtendedCreases(cp, false);
         Set<ExtendedCrease> extendedCreasesReversed = createExtendedCreases(cp, true);
+
         // construct a set of ordered circular lists of edges parting from each vertex xV
-        Set<List> xL = createList(vertices);
+        Set<List<Vertex>> xL = createList(vertices);
+
         // set of all reflection graphs
         Collection<ReflectionGraph> reflectionGraphs = reflGraph.getAllReflectionGraphs();
 
@@ -87,22 +91,28 @@ public class ExtendedCreasePattern {
         return xCP;
     }
     // TODO
-    private Set<List> createList(Set<Vertex> vertices) {
-        Set<List> lists = new HashSet<>();
+    private Set<List<Vertex>> createList(Set<Vertex> vertices) {
+        Set<List<Vertex>> lists = new HashSet<>();
         return lists;
     }
 
     // TODO
     private Set<ExtendedCrease> createExtendedCreases(CreasePattern cp, boolean reverse) {
-        Set<ExtendedCrease> xC = null;
+        Set<ExtendedCrease> xC = new HashSet<>();
+
         // if reverse == false --> xC = (v1, v2, a, false)
         // if reverse == true --> xC = (v2, v1, a, false)
+
         return xC;
     }
 
     // TODO
     private Set<Vertex> copyVertices(CreasePattern cp) {
-        Set<Vertex> vertices = null;
+        Set<Vertex> vertices = new HashSet<>();
+
+        // TODO figure out correct vertex type
+        cp.getPoints().forEach(point -> vertices.add(new Vertex(point, Vertex.Type.VIRTUAL)));
+
         return vertices;
     }
 
