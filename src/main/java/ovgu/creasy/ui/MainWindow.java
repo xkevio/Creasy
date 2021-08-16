215167fc3a80906118e51de1f40ad05fa716a865
xkevio <xkeviota@gmail.com>
2021-08-16 20:16:27 +0200

    Refactor MainWindow.java

--- src/main/java/ovgu/creasy/ui/MainWindow.java
+++ src/main/java/ovgu/creasy/ui/MainWindow.java
@@ -37,9 +37,6 @@ public class MainWindow {
     public MenuItem resetMenuItem;
 
     private OrigamiModel model;
-
-    private String filePath = "";
-    private final String lastPath = "";
     private CreasePattern cp;
 
     @FXML
@@ -53,50 +50,41 @@ public class MainWindow {
         openFileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Crease Patterns", "*.cp"));
     }
 
-    public void setHostServices(HostServices hostServices) {
-        this.hostServices = hostServices;
+    /* This makes sure that the window scales correctly
+       by binding the canvas to the window size and redrawing if
+       necessary.
+     */
+    @FXML
+    public void initialize() {
+        mainCanvas = new ResizableCanvas(canvasVBox.getWidth(), canvasVBox.getHeight());
+        mainCanvas.setManaged(false);
+        canvasVBox.getChildren().add(mainCanvas);
+
+        mainCanvas.widthProperty().bind(canvasVBox.widthProperty());
+        mainCanvas.heightProperty().bind(canvasVBox.heightProperty());
+
+        mainCanvas.widthProperty().addListener((observableValue, number, t1) -> {
+            if (cp != null) {
+                cp.drawOnCanvas(mainCanvas, 1, 1);
+            }
+        });
+
+        mainCanvas.heightProperty().addListener((observableValue, number, t1) -> {
+            if (cp != null) {
+                cp.drawOnCanvas(mainCanvas, 1, 1);
+            }
+        });
     }
 
     /**
-     * Creates new canvases in the sidebar when necessary to show
-     * the steps needed
-     *
-     * @param parent the VBox or HBox in which to add the Canvases to
-     * @param amount the amount of Canvases needed
-     * @param width  width of the Canvas
-     * @param height height of the Canvas
+     * Opens a file explorer dialogue which lets the user select
+     * .cp files and upon loading them, calls setupGUI() -- drawing the
+     * Pattern to the screen.
      */
-    private static void createCanvases(Parent parent, int amount, int width, int height) {
-        for (int i = 0; i < amount; ++i) {
-            ((Pane) parent).getChildren().add(new Canvas(width, height));
-        }
-    }
-
-    private void setupGUI(InputStream is, String filePath) {
-        cp = CreasePattern.createFromFile(is);
-        ((Stage) mainCanvas.getScene().getWindow()).setTitle(filePath + " - Creasy");
-
-        System.out.println(cp.getPoints());
-        System.out.println(cp.getCreases());
-        model = new OrigamiModel(cp);
-
-        cp.drawOnCanvas(mainCanvas, 1, 1);
-
-        // should be called when the algorithm is executed, aka once the amount of steps is known
-        createCanvases(vbox, 10, 250, 250);
-        vbox.getChildren().forEach(c -> cp.drawOnCanvas((Canvas) c, 0.5, 0.5));
-
-        // after reading the file, if the file is valid:
-        foldedModelMenuItem.setDisable(false);
-        zoomInMenuItem.setDisable(false);
-        zoomOutMenuItem.setDisable(false);
-        resetMenuItem.setDisable(false);
-    }
-
     @FXML
     public void onMenuImportAction() {
         File file = openFileChooser.showOpenDialog(mainCanvas.getScene().getWindow());
-        filePath = file.getPath();
+        var filePath = file.getPath();
 
         if (file.exists()) {
             try {
@@ -110,15 +98,21 @@ public class MainWindow {
         }
     }
 
+    /**
+     * Opens Oripa with the folded 3d model,
+     * calls foldModel() method.
+     *
+     * Opens an Alert in case of an error while folding the model.
+     * @throws Exception
+     */
     @FXML
     public void onShowFoldedModelAction() throws Exception {
         if (model == null) {
-            Alert error = new Alert(Alert.AlertType.ERROR, "There is no model to fold, perhaps it wasn't loaded correctly", ButtonType.OK);
+            Alert error = new Alert(Alert.AlertType.ERROR,
+                    "There is no model to fold, perhaps it wasn't loaded correctly", ButtonType.OK);
             error.setTitle("Model is not loaded");
             error.setHeaderText("Error while folding model");
             error.showAndWait();
-
-            // System.err.println("No Model to fold");
         } else {
             OripaFoldedModelWindow foldedModelWindow = new OripaFoldedModelWindow(model.getFinishedCp());
             if (foldedModelWindow.foldModel()) {
@@ -145,42 +139,8 @@ public class MainWindow {
         resetGUI();
     }
 
-    @FXML
-    public void initialize() {
-        mainCanvas = new ResizableCanvas(canvasVBox.getWidth(), canvasVBox.getHeight());
-        mainCanvas.setManaged(false);
-        canvasVBox.getChildren().add(mainCanvas);
-
-        mainCanvas.widthProperty().bind(canvasVBox.widthProperty());
-        mainCanvas.heightProperty().bind(canvasVBox.heightProperty());
-
-        mainCanvas.widthProperty().addListener((observableValue, number, t1) -> {
-            if (cp != null) {
-                cp.drawOnCanvas(mainCanvas, 1, 1);
-            }
-        });
-
-        mainCanvas.heightProperty().addListener((observableValue, number, t1) -> {
-            if (cp != null) {
-                cp.drawOnCanvas(mainCanvas, 1, 1);
-            }
-        });
-    }
-
-    private void resetGUI() {
-        mainCanvas.getGraphicsContext2D().clearRect(0, 0, mainCanvas.getWidth(), mainCanvas.getHeight());
-        ((Stage) mainCanvas.getScene().getWindow()).setTitle("Creasy");
-        vbox.getChildren().forEach(c -> ((Canvas) c).getGraphicsContext2D().
-                clearRect(0, 0, ((Canvas) c).getWidth(), ((Canvas) c).getHeight()));
-
-        foldedModelMenuItem.setDisable(true);
-        zoomInMenuItem.setDisable(true);
-        zoomOutMenuItem.setDisable(true);
-        resetMenuItem.setDisable(true);
-
-        cp = null;
-    }
-
+    // -------------------------
+    // Loading example files
     @FXML
     public void onLoadExampleBird() {
         InputStream is = Main.class.getResourceAsStream("example/bird.cp");
@@ -198,7 +158,13 @@ public class MainWindow {
         InputStream is = Main.class.getResourceAsStream("example/crane.cp");
         setupGUI(is, "example/crane.cp");
     }
+    // -------------------------
 
+    /**
+     * Opens an "about" dialogue which displays information about Creasy
+     * and its developers
+     * @throws IOException
+     */
     @FXML
     public void onHelpAbout() throws IOException {
         Stage stage = new Stage();
@@ -216,6 +182,70 @@ public class MainWindow {
     }
 
     @FXML
-    public void onHelpCP() {
+    public void onHelpCP() {}
+
+    /**
+     * Loads a Crease Pattern, displays it on the canvases and
+     * initializes variables
+     * @param is the InputStream that is the crease pattern file
+     * @param filePath what is displayed in the title bar of the window
+     */
+    private void setupGUI(InputStream is, String filePath) {
+        cp = CreasePattern.createFromFile(is);
+        ((Stage) mainCanvas.getScene().getWindow()).setTitle(filePath + " - Creasy");
+
+        System.out.println(cp.getPoints());
+        System.out.println(cp.getCreases());
+        model = new OrigamiModel(cp);
+
+        cp.drawOnCanvas(mainCanvas, 1, 1);
+
+        // should be called when the algorithm is executed, aka once the amount of steps is known
+        createCanvases(vbox, 10, 250, 250);
+        vbox.getChildren().forEach(c -> cp.drawOnCanvas((Canvas) c, 0.5, 0.5));
+
+        // after reading the file, if the file is valid:
+        foldedModelMenuItem.setDisable(false);
+        zoomInMenuItem.setDisable(false);
+        zoomOutMenuItem.setDisable(false);
+        resetMenuItem.setDisable(false);
+    }
+
+    /**
+     * Clears all the canvases and disables menu options that would only work
+     * with a loaded file.
+     * Useful for resetting state.
+     */
+    private void resetGUI() {
+        mainCanvas.getGraphicsContext2D().clearRect(0, 0, mainCanvas.getWidth(), mainCanvas.getHeight());
+        ((Stage) mainCanvas.getScene().getWindow()).setTitle("Creasy");
+        vbox.getChildren().forEach(c -> ((Canvas) c).getGraphicsContext2D().
+                clearRect(0, 0, ((Canvas) c).getWidth(), ((Canvas) c).getHeight()));
+
+        foldedModelMenuItem.setDisable(true);
+        zoomInMenuItem.setDisable(true);
+        zoomOutMenuItem.setDisable(true);
+        resetMenuItem.setDisable(true);
+
+        cp = null;
+    }
+
+    public void setHostServices(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+    /**
+     * Creates new canvases in the sidebar when necessary to show
+     * the steps needed
+     *
+     * @param parent the VBox or HBox in which to add the Canvases to
+     * @param amount the amount of Canvases needed
+     * @param width  width of the Canvas
+     * @param height height of the Canvas
+     */
+    private static void createCanvases(Parent parent, int amount, int width, int height) {
+        for (int i = 0; i < amount; ++i) {
+            ((Pane) parent).getChildren().add(new Canvas(width, height));
+        }
     }
 }
