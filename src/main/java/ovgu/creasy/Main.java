64889043c605ead97fd00f30413463cefda959b8
xkevio <xkeviota@gmail.com>
2021-08-30 21:54:16 +0200

    Remove unused import

--- src/main/java/ovgu/creasy/Main.java
+++ src/main/java/ovgu/creasy/Main.java
@@ -1,7 +1,6 @@
 package ovgu.creasy;
 
 import javafx.application.Application;
-import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
