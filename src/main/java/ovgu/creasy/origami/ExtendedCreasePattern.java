855a2e6ee7dc1b8b1a977b103324aa92314c1349
undertrox <undertrox@gmail.com>
2021-09-06 14:50:57 +0200

    refactor ExtendedCreasePattern

--- src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
+++ src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
@@ -10,19 +10,19 @@ import java.util.*;
  * Explained in chapter 4.2 of the paper (pages 28 - 36)
  */
 public class ExtendedCreasePattern {
-    private Set<Vertex> xV;
-    private Set<ExtendedCrease> xC;
-    private Map<Vertex, List<ExtendedCrease>> xL;
+    private Set<Vertex> vertices;
+    private Set<ExtendedCrease> creases;
+    private Map<Vertex, List<ExtendedCrease>> connections;
     private CreasePattern cp;
     /**
-     * @param xV is the set of extended vertices
-     * @param xC is the set of directed edges of the extended graph. Each xC ist called extended creases
-     * @param xL is a set of ordered, circular lists of edges parting from each vertex xV
+     * @param vertices is the set of extended vertices
+     * @param creases is the set of directed edges of the extended graph.
+     * @param connections is a set of ordered, circular lists of edges parting from each vertex in vertices
      */
-    public ExtendedCreasePattern(Set<Vertex> xV, Set<ExtendedCrease> xC, Map<Vertex, List<ExtendedCrease>> xL, CreasePattern cp) {
-        this.xV = xV;
-        this.xC = xC;
-        this.xL = xL;
+    public ExtendedCreasePattern(Set<Vertex> vertices, Set<ExtendedCrease> creases, Map<Vertex, List<ExtendedCrease>> connections, CreasePattern cp) {
+        this.vertices = vertices;
+        this.creases = creases;
+        this.connections = connections;
         this.cp = cp;
     }
 
@@ -34,55 +34,27 @@ public class ExtendedCreasePattern {
     }
 
     public Set<Vertex> getVertices() {
-        return Collections.unmodifiableSet(xV);
+        return Collections.unmodifiableSet(vertices);
     }
 
     public Set<ExtendedCrease> getCreases() {
-        return Collections.unmodifiableSet(xC);
+        return Collections.unmodifiableSet(creases);
     }
 
-    public Map<Vertex, List<ExtendedCrease>> getAdjacencyLists() { return Collections.unmodifiableMap(xL); }
+    public Map<Vertex, List<ExtendedCrease>> getAdjacencyLists() { return Collections.unmodifiableMap(connections); }
 
     @Override
     public String toString() {
         return "ExtendedCreasePattern{" +
-            "xC=" + xC +
+            "xC=" + creases +
             '}';
     }
 
     public List<DiagramStep> possibleSteps() {
         List<DiagramStep> steps = new ArrayList<>();
         HashSet<ReflectionPath> removableCreases = new HashSet<>();
-        Map<Vertex, List<ExtendedCrease>> possiblyRemovableCreases = new HashMap<>();
-        for (Vertex vertex : xV) {
-            if (vertex.getType() == Vertex.Type.BORDER) {
-                List<ExtendedCrease> outgoing = this.xL.get(vertex);
-                for (ExtendedCrease outgoingCrease : outgoing) {
-                    if (outgoingCrease.getType() == Crease.Type.EDGE) {
-                        continue;
-                    }
-                    if (outgoingCrease.getEndVertex().getType() == Vertex.Type.BORDER
-                        && outgoingCrease.getType()!= Crease.Type.EDGE) {
-                        removableCreases.add(outgoingCrease.getReflectionPath());
-                    } else if (outgoingCrease.getEndVertex().getType() == Vertex.Type.VIRTUAL) {
-                        List<ExtendedCrease> creases = new ArrayList<>();
-                        creases.add(outgoingCrease);
-                        ExtendedCrease currentCrease = outgoingCrease;
-                        while (xL.containsKey(currentCrease.getEndVertex())) {
-                            var next = xL.get(currentCrease.getEndVertex());
-                            currentCrease = next.get(next.size()-1);
-                            creases.add(currentCrease);
-                        }
-                        Vertex middle = currentCrease.getEndVertex();
-                        if (possiblyRemovableCreases.containsKey(middle)) {
-                            possiblyRemovableCreases.get(middle).addAll(creases);
-                            removableCreases.add(currentCrease.getReflectionPath());
-                        } else {
-                            possiblyRemovableCreases.put(middle, creases);
-                        }
-                    }
-                }
-            }
+        for (Vertex vertex : vertices) {
+            removableCreases.addAll(findSimpleFolds(vertex));
         }
         for (ReflectionPath removableCreaseList : removableCreases) {
             CreasePattern newcp = this.cp.copy();
@@ -92,5 +64,38 @@ public class ExtendedCreasePattern {
             steps.add(new DiagramStep(this, next));
         }
         return steps;
+    }
+
+    private HashSet<ReflectionPath> findSimpleFolds(Vertex vertex) {
+        Map<Vertex, ReflectionPath> possiblySimpleFolds = new HashMap<>();
+        HashSet<ReflectionPath> simpleFolds = new HashSet<>();
+        if (vertex.getType() == Vertex.Type.BORDER) {
+            List<ExtendedCrease> outgoing = this.connections.get(vertex);
+            for (ExtendedCrease outgoingCrease : outgoing) {
+                if (outgoingCrease.getType() == Crease.Type.EDGE) {
+                    continue;
+                }
+                if (outgoingCrease.getEndVertex().getType() == Vertex.Type.BORDER
+                    && outgoingCrease.getType()!= Crease.Type.EDGE) {
+                    simpleFolds.add(outgoingCrease.getReflectionPath());
+                } else if (outgoingCrease.getEndVertex().getType() == Vertex.Type.VIRTUAL) {
+                    List<ExtendedCrease> creases = new ArrayList<>();
+                    creases.add(outgoingCrease);
+                    ExtendedCrease currentCrease = outgoingCrease;
+                    while (connections.containsKey(currentCrease.getEndVertex())) {
+                        var next = connections.get(currentCrease.getEndVertex());
+                        currentCrease = next.get(next.size()-1);
+                        creases.add(currentCrease);
+                    }
+                    Vertex middle = currentCrease.getStartVertex();
+                    if (possiblySimpleFolds.containsKey(middle)) {
+                        simpleFolds.add(currentCrease.getReflectionPath());
+                    } else {
+                        possiblySimpleFolds.put(middle, outgoingCrease.getReflectionPath());
+                    }
+                }
+            }
+        }
+        return simpleFolds;
     }
 }
