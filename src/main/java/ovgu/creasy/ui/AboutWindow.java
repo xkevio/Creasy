eee4fb98902bf45613d4029ced2f8c2cc12a68dd
xkevio <xkeviota@gmail.com>
2021-09-14 02:37:21 +0200

    Add GPL3 license to about window

--- src/main/java/ovgu/creasy/ui/AboutWindow.java
+++ src/main/java/ovgu/creasy/ui/AboutWindow.java
@@ -3,7 +3,9 @@ package ovgu.creasy.ui;
 import javafx.application.HostServices;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
@@ -28,16 +30,37 @@ public class AboutWindow {
         Hyperlink gitHub = new Hyperlink("https://github.com/xkevio/Creasy");
         gitHub.setOnAction(e -> hostServices.showDocument("https://github.com/xkevio/Creasy"));
 
-        about.getDialogPane().setContent(new TextFlow(
-                new Text("""
-                Creasy is a software that allows you to convert
-                crease patterns into simple step-by-step instructions.
-                
-                Find us here:\040"""),
-                gitHub));
+        TextArea license = new TextArea();
+        license.setText("""
+                        Creasy - a software to turn crease patterns into instructions
+                        Copyright © 2021  xkevio, undertrox, Skogsbaer, RxZer
+                                    
+                        This program is free software: you can redistribute it and/or modify
+                        it under the terms of the GNU General Public License as published by
+                        the Free Software Foundation, either version 3 of the License, or
+                        (at your option) any later version.
+                                    
+                        This program is distributed in the hope that it will be useful,
+                        but WITHOUT ANY WARRANTY; without even the implied warranty of
+                        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                        GNU General Public License for more details.
+                                    
+                        You should have received a copy of the GNU General Public License
+                        along with this program.  If not, see https://www.gnu.org/licenses.
+                        """);
+        license.setWrapText(true);
+        license.setEditable(false);
+
+        about.getDialogPane().setContent(new VBox(
+                new TextFlow(
+                    new Text("""
+                    Creasy is a software that allows you to convert
+                    crease patterns into simple step-by-step instructions.
+                    
+                    Find us here:"""),
+                gitHub, new Text("\n")), license));
 
         about.showAndWait();
-
     }
 }
 
