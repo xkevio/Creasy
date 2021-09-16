1d305f742a60239e02750d1608df04815d5c5e9c
xkevio <xkeviota@gmail.com>
2021-09-16 11:59:31 +0200

    Add abstract `open()` method to exporters to open save dialogue and return the file

--- src/main/java/ovgu/creasy/util/AbstractExporter.java
+++ src/main/java/ovgu/creasy/util/AbstractExporter.java
@@ -2,10 +2,12 @@ package ovgu.creasy.util;
 
 import ovgu.creasy.ui.ResizableCanvas;
 
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
 
 public abstract class AbstractExporter {
-
     protected List<ResizableCanvas> history;
+    public abstract Optional<File> open();
     public abstract boolean export();
 }
--- src/main/java/ovgu/creasy/util/SVGExporter.java
+++ src/main/java/ovgu/creasy/util/SVGExporter.java
@@ -2,12 +2,19 @@ package ovgu.creasy.util;
 
 import ovgu.creasy.ui.ResizableCanvas;
 
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
 
 public class SVGExporter extends AbstractExporter {
 
     public SVGExporter(List<ResizableCanvas> history) {
         this.history = history;
+    }
+
+    @Override
+    public Optional<File> open() {
+        return Optional.empty();
     }
 
     @Override
